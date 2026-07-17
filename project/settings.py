baf1a8a75882a9bb16ecc1aecdec94aa1687517e
Lucas Sekikawa <lucassekikawa82@gmail.com>
2026-07-17 09:32:16 -0300

    models created and more configs

--- project/settings.py
+++ project/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,3 +121,14 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+ 
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...

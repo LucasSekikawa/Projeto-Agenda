249030f70ffcd594d534725af8a46ac1838daf6f
Lucas Sekikawa <lucassekikawa82@gmail.com>
2026-07-22 08:31:52 -0300

    mais adds

--- contact/forms.py
+++ contact/forms.py
@@ -3,20 +3,21 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept':'image/*'
+            }
+        )
+    )
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone', 
             'email', 'description', 'category',
-        ) 
-
-        widgets = {
-            'first_name': forms.TextInput(
-                attrs={
-                    'placeholder': 'Escreva aqui'
-                }
-            )
-        }
+            'picture',
+        )
 
     def clean(self):
         cleaned_data = self.cleaned_data
--- contact/urls.py
+++ contact/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 
     # CRUD
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    path('contact/create/', views.create, name='create')
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete')
 ]
